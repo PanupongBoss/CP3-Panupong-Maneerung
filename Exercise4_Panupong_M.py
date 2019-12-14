3661c75ef962e246c3b7c4d6122af5d2d4c410ff
panupongboss <panupong32@github.com>
2019-12-14 12:20:29 +0700

    Add Exercise 4
    Exercise4

---
+++ Exercise4_Panupong_M.py
@@ -0,0 +1,10 @@
+FE = 70
+GB = 75
+ICS = 88.5
+CP = 95.5
+print("-------------------Your Score-------------------")
+print("Foundation English               :" ,FE)
+print("General Business                 :" ,GB)
+print("Introduction to Computer Systems :" ,ICS)
+print("Computer Programming             :" ,CP)
+print("------------------------------------------------")
