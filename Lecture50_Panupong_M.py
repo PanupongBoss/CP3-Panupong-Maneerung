b338d89748d6a69b056407aba22399025d0a254c
panupongboss <panupong32@github.com>
2019-12-27 15:21:26 +0700

    Create Lecture50_Panupong_M.py

---
+++ Lecture50_Panupong_M.py
@@ -0,0 +1,12 @@
+def sumNumber(x,y):
+    print(x+y)
+def delNumber(x,y):
+    print(x-y)
+def mulNumber(x,y):
+    print(x*y)
+def divNumber(x,y):
+    print(int(x/y))
+sumNumber(100,25)
+delNumber(100,25)
+mulNumber(100,25)
+divNumber(100,25)
